62537369d3b570fda26624a5e853f0409be67542
Chuan Ji <chuan@jichu4n.com>
2018-11-02 00:58:21 -0700

    Start work on options view.

--- src/options/options-view.tsx
+++ src/options/options-view.tsx
@@ -1,3 +1,36 @@
+import AppBar from '@material-ui/core/AppBar';
+import yellow from '@material-ui/core/colors/yellow';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
 
-export default () => <div>Options!</div>;
+const APP_BAR_THEME = createMuiTheme({
+  palette: {
+    primary: {
+      main: yellow[800],
+      contrastText: '#fff',
+    },
+  },
+});
+
+class OptionsView extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider theme={APP_BAR_THEME}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" color="inherit">
+                Agentium Settings
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+
+export default OptionsView;
