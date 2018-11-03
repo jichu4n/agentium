bac01b7b87831dd864b29250577676758ee6efc7
Chuan Ji <chuan@jichu4n.com>
2018-11-03 01:49:44 -0700

    Update default user agent list.

--- src/state/default-ua-spec-list.ts
+++ src/state/default-ua-spec-list.ts
@@ -47,7 +47,7 @@ const DEFAULT_UA_SPEC_LIST: Array<UaSpec> = [
     deviceType: 'mobile',
     name: 'Microsoft Edge (Windows Phone 10)',
     value:
-      'Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1; Microsoft; Lumia 650) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15063',
+      'Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1; Microsoft; Lumia 650) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15254',
   },
   {
     deviceType: 'tablet',
